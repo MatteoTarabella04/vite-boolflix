869f7f848d24b4b45b150a8ee99b22e12e167fd8
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-19 12:25:20 +0200

    add api url and call axios

--- src/store.js
+++ src/store.js
@@ -2,5 +2,21 @@ import { reactive } from "vue";
 import axios from "axios";
 
 export const store = reactive({
+   searchText: '',
+   API_FILMS_URL: 'https://api.themoviedb.org/3/search/movie?api_key=7be289e2af34e97e1f3298980f1e7502',
+   films: null,
 
+   callApi(url) {
+      axios
+         .get(url)
+         .then(response => {
+            console.log(response);
+            this.films = response.data.results;
+            console.log(this.films);
+         })
+         .catch(err => {
+            console.log(err);
+            console.error(err.message);
+         })
+   },
 })
