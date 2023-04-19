113157d82d25ba87c619abde8ce8be3a482a1a21
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-19 17:28:39 +0200

    add more flags conditions

--- src/components/FilmsList.vue
+++ src/components/FilmsList.vue
@@ -14,10 +14,19 @@ export default {
 </script>
 <template>
    <ul v-for="film in this.store.films">
-      <li> <b>Title: {{ film.title }}</b> </li>
-      <li>Original Title: {{ film.original_title }}</li>
+      <li v-if="film.title == 'title'"> <b>Title: {{ film.title }}</b> </li>
+      <li v-else> <b>Title: {{ film.name }}</b> </li>
+      <li v-if="film.original_title == 'original_title'"> Original Title: {{ film.original_title }}</li>
+      <li v-else>Original Title: {{ film.original_name }} </li>
+      <!-- <li>Original Title: {{ film.original_title }}</li> -->
       <li v-if="film.original_language == 'en'">
          Original Language: <img src="https://flagcdn.com/w20/gb.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'ar'">
+         Original Language: <img src="https://flagcdn.com/w20/ae.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'el'">
+         Original Language: <img src="https://flagcdn.com/w20/gr.png" width="20" :alt="film.original_language">
       </li>
       <li v-else-if="film.original_language == 'ja'">
          Original Language: <img src="https://flagcdn.com/w20/jp.png" width="20" :alt="film.original_language">
