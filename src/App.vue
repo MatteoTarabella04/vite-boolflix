2450a2dcbc8571693685d8d33b383733ab904ff0
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-19 16:47:37 +0200

    add flags language

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
       console.log(this.store.searchText);
       const url = this.store.API_FILMS_URL + `&query=${this.store.searchText}`;
       this.store.callApi(url);
-    }
+    },
   },
   mounted() {
     this.store.callApi(store.API_FILMS_URL)
@@ -34,7 +34,32 @@ export default {
     <ul v-for="film in this.store.films">
       <li> <b>Title: {{ film.title }}</b> </li>
       <li>Original Title: {{ film.original_title }}</li>
-      <li>Original Language: {{ film.original_language }}</li>
+      <li v-if="film.original_language == 'en'">
+        Original Language: <img src="https://flagcdn.com/w20/gb.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'ja'">
+        Original Language: <img src="https://flagcdn.com/w20/jp.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'zh'">
+        Original Language: <img src="https://flagcdn.com/w20/cn.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'ko'">
+        Original Language: <img src="https://flagcdn.com/w20/kr.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'ta'">
+        Original Language: <img src="https://flagcdn.com/w20/in.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'hi'">
+        Original Language: <img src="https://flagcdn.com/w20/in.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else-if="film.original_language == 'uk'">
+        Original Language: <img src="https://flagcdn.com/w20/ua.png" width="20" :alt="film.original_language">
+      </li>
+      <li v-else>
+        Original Language: <img :src="`https://flagcdn.com/w20/${film.original_language}.png`" width="20"
+          :alt="film.original_language">
+      </li>
+      <!-- <li v-else>Original Language: {{ film.original_language }}</li> -->
       <li>Vote: {{ film.vote_average }}</li>
     </ul>
   </div>
