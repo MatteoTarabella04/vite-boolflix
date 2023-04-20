0d88ef0af4851d6de112221b627bc94c669cd408
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-20 14:02:18 +0200

    cvreate app amain and add dilm list component

---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+import FilmsList from './FilmsList.vue';
+export default {
+
+   name: 'AppMain',
+
+   components: { FilmsList, },
+
+   data() {
+      return {
+      }
+   },
+}
+</script>
+<template>
+   <FilmsList />
+</template>
+
+
+<style lang="scss" scoped></style>
