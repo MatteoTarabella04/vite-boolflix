d81d7a8d15955c5d98172f44cd0a6c7f25739284
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-20 17:13:27 +0200

    add dropdowns +modify search input

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -15,7 +15,7 @@ export default {
 <template>
    <div class="searchBox d-flex gap-4 align-items-center">
       <div class="input-group">
-         <input type="text" class="form-control bg-dark border-danger text-light" placeholder="Search a film..."
+         <input type="text" class="form-control bg-dark border-danger text-light" placeholder="Search"
             v-model="store.searchText">
          <button class="btn btn-outline-danger" type="button" @click="$emit('search')">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
@@ -25,20 +25,51 @@ export default {
             </svg>
          </button>
       </div>
-      <div class="bell">
-         <a href="#" class="text-white">
+      <div class="bell dropdown">
+         <button id="dropdownBell" class="btn text-white" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell"
                viewBox="0 0 16 16">
                <path
                   d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
             </svg>
-         </a>
+         </button>
+         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownBell">
+            <li><a href="#" class="dropdown-item">News</a></li>
+            <li><a href="#" class="dropdown-item">News</a></li>
+            <li><a href="#" class="dropdown-item">News</a></li>
+         </ul>
       </div>
-      <div class="profile">
-         <img src="../assets/img/avatar_batman.png" alt="" class="img-fluid">
+      <div class="profile dropdown">
+         <button id="dropdownProfile" class="btn text-white p-0">
+            <img src="../assets/img/avatar_batman.png" alt="avatar" class="img-fluid">
+         </button>
+         <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownProfile">
+            <li class="d-flex">
+               <a href="#" class="dropdown-item">
+                  <img src="../assets/img/avatar_batman.png" alt="avatar" width="30" class="img-fluid">
+                  Profile 1
+               </a>
+            </li>
+            <li class="d-flex">
+               <a href="#" class="dropdown-item">
+                  <img src="../assets/img/avatar_batman.png" alt="avatar" width="30" class="img-fluid">
+                  Profile 2
+               </a>
+            </li>
+            <li class="d-flex">
+               <a href="#" class="dropdown-item">
+                  <img src="../assets/img/avatar_batman.png" alt="avatar" width="30" class="img-fluid">
+                  Profile 3
+               </a>
+            </li>
+         </ul>
       </div>
    </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dropdown:hover>.dropdown-menu {
+   display: block;
+}
+</style>
