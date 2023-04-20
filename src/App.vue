1921ce8955a615a940ce0d633820cfb51aa6a874
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-20 14:00:51 +0200

    import and add mani and header components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <script>
-import AppMain from './AppMain.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 export default {
 
-  components: { AppMain, },
+  components: { AppMain, AppHeader },
 
-
+  data() {
+    return {
+    }
+  }
 
 }
 </script>
 <template>
+  <AppHeader />
   <AppMain />
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#app_header {
+  height: 60px;
+}
+</style>
