bf2473efc137ac355b101e1832474b49ac67c6e8
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-20 11:05:56 +0200

    add call for series and array

--- src/store.js
+++ src/store.js
@@ -7,14 +7,28 @@ export const store = reactive({
    API_SERIES_URL: 'https://api.themoviedb.org/3/search/tv?api_key=7be289e2af34e97e1f3298980f1e7502',
    posterPath: 'https://image.tmdb.org/t/p/w342/',
    films: null,
+   series: null,
 
-   callApi(url) {
+   callApiFilms(url) {
       axios
          .get(url)
          .then(response => {
             console.log(response);
             this.films = response.data.results;
             console.log(this.films);
+         })
+         .catch(err => {
+            console.log(err);
+            console.error(err.message);
+         })
+   },
+   callApiSeries(url) {
+      axios
+         .get(url)
+         .then(response => {
+            console.log(response);
+            this.series = response.data.results;
+            console.log(this.series);
          })
          .catch(err => {
             console.log(err);
