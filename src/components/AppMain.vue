24b989743b25d36218c2ac202abc7bac6944bdfd
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-21 12:41:46 +0200

    add style to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,11 +13,16 @@ export default {
 }
 </script>
 <template>
-   <main id="app_main">
+   <main id="app_main" class="bg-dark">
       <FilmsList />
    </main>
    <!-- /#app_main -->
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#app_main {
+   height: calc(100vh - 60px);
+   overflow-y: auto;
+}
+</style>
