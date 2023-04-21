a82f2772d067dcaecaa3eba9e8f1d38cd9138cba
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-21 12:43:15 +0200

    add a section for movies and series

--- src/components/FilmsList.vue
+++ src/components/FilmsList.vue
@@ -1,9 +1,10 @@
 <script>
 import { store } from '../store';
 import FilmItem from './FilmItem.vue';
+import SeriesItem from './SeriesItem.vue';
 export default {
 
-   components: { FilmItem, },
+   components: { FilmItem, SeriesItem },
 
    data() {
       return {
@@ -15,11 +16,24 @@ export default {
 }
 </script>
 <template>
-   <div class="container">
-      <div class="row row-cols-5">
-         <FilmItem />
+   <section id="movies_sec">
+      <div class="container">
+         <h2 class="text-light text-center">Movies</h2>
+         <div class="row row-cols-5">
+            <FilmItem />
+         </div>
       </div>
-   </div>
+   </section>
+   <!-- /#movies_sec -->
+   <section id="series_sec">
+      <div class="container">
+         <h2 class="text-light text-center">Series</h2>
+         <div class="row row-cols-5">
+            <SeriesItem />
+         </div>
+      </div>
+   </section>
+   <!-- /#series_sec -->
 </template>
 
 
